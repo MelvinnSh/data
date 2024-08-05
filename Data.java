b3e5df23d46ffd851304439b81883ea908c4c206
MelvinnSh <melvin_ismiananta_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:45:13 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,14 @@
+public class Data {
+    public static void main(String[] args) {
+        //tipe_data nama_var = valie/isi var;
+        String nama =  "Melvin";
+        int usia = 15;
+        String alamat = "JL Danau Towuti IV G5 D12";
+        double kelas = 10.7;
+
+System.out.println("nama saya : " + nama);
+System.out.println("usia : " + usia + " tahun ");
+System.out.println("alamat : " + alamat);
+System.out.println("kelas : " + kelas);
+    }
+}
