30231fd8f32b1994f8b37eb3e1a3780d98e9f800
MelvinnSh <melvin_ismiananta_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 14:05:20 +0700

    Add files via upload

---
+++ SiswaTester.java
@@ -0,0 +1,57 @@
+//Driver Class
+public class SiswaTester {
+    public static void main(String[] args) {
+        //object
+        //Class object = new Constructor
+        Siswa averil = new Siswa();
+        Siswa ryuichi = new Siswa();
+        Siswa bima = new Siswa();
+        Siswa naufal = new Siswa();
+        Siswa rafli = new Siswa();
+
+        averil.id = 5;
+        averil.nama = "averil";
+        averil.ipk = 7.0;
+
+        System.out.println("absenn : " + averil.id);
+        System.out.println("namaa : " + averil.nama);
+        System.out.println("ipk nyaa : " + averil.ipk);
+        System.out.println("   ");
+
+        ryuichi.id = 32;
+        ryuichi.nama = "ryuichi";
+        ryuichi.ipk = 2.30;
+
+        System.out.println("absenn : " + ryuichi.id);
+        System.out.println("namaa : " + ryuichi.nama);
+        System.out.println("ipk nyaa : " + ryuichi.ipk);
+        System.out.println("   ");
+        bima.id = 2;
+        bima.nama = "bima";
+        bima.ipk = 1.20;
+
+        System.out.println("absenn : " + bima.id);
+        System.out.println("namaa : " + bima.nama);
+        System.out.println("ipk nyaa : " + bima.ipk);
+        System.out.println("   ");
+
+        naufal.id = 28;
+        naufal.nama = "naufal";
+        naufal.ipk = 4.78;
+
+        System.out.println("absenn : " + naufal.id);
+        System.out.println("namaa : " + naufal.nama);
+        System.out.println("ipk nyaa : " + naufal.ipk);
+        System.out.println("   ");
+
+        rafli.id = 5;
+        rafli.nama = "rafli";
+        rafli.ipk = 3.90;
+
+        System.out.println("absenn : " + rafli.id);
+        System.out.println("namaa : " + rafli.nama);
+        System.out.println("ipk nyaa : " + rafli.ipk);
+        System.out.println("   ");
+
+    }
+}
